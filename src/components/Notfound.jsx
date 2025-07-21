88984c47e527e8ec8e0ecde09f0e5d197edc78b0
Yousef Reda <yossefr95@gmail.com>
2025-07-21 18:51:19 +0300

    update Notfound component

--- src/components/Notfound.jsx
+++ src/components/Notfound.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Notfound() {
     return (
-        <div>Notfound</div>
+        <div style={{ height: '100vh', margin: '150px ', fontWeight: 'bold', fontSize: '40px' }}>
+            Not Found
+        </div>
     )
 }
